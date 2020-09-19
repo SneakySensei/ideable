b13967a855bf947e376645e1805e46eb39ab314c
Snehil <snehilwebber@gmail.com>
2020-09-20 02:45:40 +0530

    Add mongoose db connection

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const dbCreds = require("./dbCreds");
+
+// Replace this with your MONGOURI.
+const MONGOURI = `mongodb+srv://ideableAdmin:${
+  process.env.dbPass || dbCreds.password
+}@cluster0.pptuz.azure.mongodb.net/ideableDB?retryWrites=true&w=majority`;
+
+const InitiateMongoServer = async () => {
+  try {
+    await mongoose.connect(MONGOURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to DB !!");
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+module.exports = InitiateMongoServer;
